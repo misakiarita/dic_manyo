7cf08298fe77f350c5e9b23e87efe4eb10e690b8
misaki arita <misaki.arita0838@gmail.com>
2023-01-05 23:13:13 +0900

    作業ステータスの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:content, :title, :due)
+    params.require(:task).permit(:content, :title, :due, :status)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_05_064801) do
+ActiveRecord::Schema.define(version: 2023_01_05_123757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2023_01_05_064801) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "due", default: -> { "now()" }, null: false
+    t.integer "status", default: 0, null: false
   end
 
 end
