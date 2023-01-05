cb1b23ca45da21164c516bc6532998609b0e6a14
misaki arita <misaki.arita0838@gmail.com>
2023-01-05 17:31:59 +0900

    終了期限をshowファイルへ記載

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,7 +46,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:content, :title)
+    params.require(:task).permit(:content, :title, :due)
   end
 
 end
