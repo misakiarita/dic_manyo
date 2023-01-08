f129dd61c5260b94b17c4be323749263885dbb75
misaki arita <misaki.arita0838@gmail.com>
2023-01-08 20:57:38 +0900

    システムスペックのエラー解消

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, title: 'タイトル1', content: 'コンテント1')
+        task = FactoryBot.create(:task, title: 'タイトル1', content: 'タスク1')
         visit tasks_path
         expect(page).to have_content 'タイトル1'
       end
