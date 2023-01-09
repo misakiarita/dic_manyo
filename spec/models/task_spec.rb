352da69825b8e33563a7011277e3115182dd269b
misaki arita <misaki.arita0838@gmail.com>
2023-01-09 16:14:20 +0900

    systemspecのRspecを実装

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,39 +2,33 @@ require 'rails_helper'
 
 RSpec.describe 'タスクモデル機能', type: :model do
 
-  # describe 'バリデーションのテスト' do
-  #   context 'タスクのタイトルが空の場合' do
-  #     it 'バリデーションにひっかる' do
-  #       task = Task.new(title: '', content: '失敗テスト')
-  #       expect(task).not_to be_valid
-  #     end
-  #   end
-  #   context 'タスクの詳細が空の場合' do
-  #     it 'バリデーションにひっかかる' do
-  #       task = Task.new(title: '失敗テスト', content: '')
-  #       expect(task).not_to be_valid
-  #     end
-  #   end
-  #   context 'タスクのタイトルと詳細に内容が記載されている場合' do
-  #     it 'バリデーションが通る' do
-  #       task = Task.new(title: 'テスト', content: 'テスト')
-  #       expect(task).to be_valid
-  #     end
-  #   end
-  # end
+  describe 'バリデーションのテスト' do
+    context 'タスクのタイトルが空の場合' do
+      it 'バリデーションにひっかる' do
+        task = Task.new(title: '', content: '失敗テスト')
+        expect(task).not_to be_valid
+      end
+    end
+    context 'タスクの詳細が空の場合' do
+      it 'バリデーションにひっかかる' do
+        task = Task.new(title: '失敗テスト', content: '')
+        expect(task).not_to be_valid
+      end
+    end
+    context 'タスクのタイトルと詳細に内容が記載されている場合' do
+      it 'バリデーションが通る' do
+        task = Task.new(title: 'テスト', content: 'テスト')
+        expect(task).to be_valid
+      end
+    end
+  end
 
   describe '検索機能' do
     let!(:task) { FactoryBot.create(:task, title: 'task') }
     let!(:second_task) { FactoryBot.create(:second_task, title: "sample") }
     let!(:third_task) { FactoryBot.create(:third_task, title: "sample") }
 
-    # before do
-    #   let!(:task) { FactoryBot.create(:task, title: 'task') }
-    #   let!(:second_task) { FactoryBot.create(:second_task, title: "sample") }
-
-    # end
-
-    context 'タイトルであいまい検索をした場合' do
+    context 'scopeメソッドでタイトルのあいまい検索をした場合'  do
       it "検索キーワードを含むタスクで絞り込まれる" do
       
         expect(Task.title_search('task')).to include(task)
@@ -46,23 +40,19 @@ RSpec.describe 'タスクモデル機能', type: :model do
         # expect(Task.title_search(params)).to include(task)
         # expect(Task.title_search(params)).not_to include(second_task)
         # expect(Task.title_search(params).count).to eq 1
-   
-
-   
+      
       end
     end
-    context 'ステータス検索をした場合' do
+    context 'scopeメソッドでステータス検索をした場合' do
       it "ステータスに完全一致するタスクが絞り込まれる" do
 
         expect(Task.status_search(1)).to include(second_task)
         expect(Task.title_search(1)).not_to include(task)
         expect(Task.status_search(1).count).to eq 2
-
-        # ここに実装する
-        # プルダウンを選択する「select」について調べてみること
+       
       end
     end
-    context 'タイトルのあいまい検索とステータス検索をした場合' do
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
       it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
         expect(Task.status_search(1).title_search('sample')).to include(second_task)
         expect(Task.status_search(1).title_search('sample')).not_to include(task)
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,10 +3,10 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
-      visit new_task_path
-      fill_in 'task[content]', with: 'task'    
-      click_on '登録する'
-      expect(page).to have_content 'task'
+        visit new_task_path
+        fill_in 'task[content]', with: 'task'    
+        click_on '登録する'
+        expect(page).to have_content 'task'
       end
     end
   end
@@ -22,29 +22,30 @@ RSpec.describe 'タスク管理機能', type: :system do
 
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
-      task = FactoryBot.create(:task, title: 'タイトル1')
-      task = FactoryBot.create(:second_task, title: 'タイトル2')
+        task = FactoryBot.create(:task, title: 'タイトル1')
+        task = FactoryBot.create(:second_task, title: 'タイトル2')
 
-      visit tasks_path
-      task_list = all('.task_row')
-      expect(task_list[0]).to have_content 'タイトル2'
-      expect(task_list[1]).to have_content 'タイトル1'
+        visit tasks_path
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'タイトル2'
+        expect(task_list[1]).to have_content 'タイトル1'
       end
     end
 
     context 'タスクが終了期限の降順に並んでいる場合' do
       it '終了期限が近いものから表示する' do
-      task = FactoryBot.create(:task, title: 'タイトル1', due: "2023-02-10 11:48:00")
-      task = FactoryBot.create(:second_task, title: 'タイトル2', due: "2023-02-07 11:48:00")
-      visit tasks_path
-      click_on '終了期限でソートする'
-      task_list = all('.task_row')
-      expect(task_list[0]).to have_content 'タイトル1'
-      expect(task_list[1]).to have_content 'タイトル2'
+        task = FactoryBot.create(:task, title: 'タイトル1', due: "2023-02-10 11:48:00")
+        task = FactoryBot.create(:second_task, title: 'タイトル2', due: "2023-02-07 11:48:00")
+        visit tasks_path
+        click_on '終了期限でソートする'
+        sleep(2)
+        task_list = all('.task_row')
+          expect(task_list[0]).to have_content 'タイトル1'
+          expect(task_list[1]).to have_content 'タイトル2'
       end
     end
-
-   end
+  end
+   
 
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
@@ -55,5 +56,39 @@ RSpec.describe 'タスク管理機能', type: :system do
           expect(page).to have_content 'task'
         end
      end     
+  end
+
+  describe '検索機能' do
+    before do
+      FactoryBot.create(:task, title: "task", status: "未着手")
+      FactoryBot.create(:second_task, title: "sample", status: "着手中")
+    end
+
+    context 'タイトルであいまい検索をした場合' do
+      it "検索キーワードを含むタスクで絞り込まれる" do
+        visit tasks_path
+        fill_in 'task[title]', with: 'task'
+        click_on '検索'
+        expect(page).to have_content 'task'
+      end
+    end
+    context 'ステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        visit tasks_path
+        select '未着手', from: 'task_status'
+        click_on '検索'
+        expect(page).to have_content '未着手'
+
+      end
+    end
+    context 'タイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        visit tasks_path
+        fill_in 'task[title]', with: 'task'
+        select '未着手', from: 'task_status'
+        click_on '検索'
+        expect(page).to have_content '未着手'
+      end
+    end
   end
 end
