1e5deb0e2a8b27675d95c3ffb2a0678549809536
misaki arita <misaki.arita0838@gmail.com>
2023-01-11 11:57:00 +0900

    sign up機能の実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'misaki', email: 'misaki@gmail.com', 
+            password: 'misaki', password_confirmation: 'misaki')
