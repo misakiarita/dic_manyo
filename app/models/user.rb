a684f961d3807336c9f689fc9f29b867f1caafac
misaki arita <misaki.arita0838@gmail.com>
2023-01-12 15:59:15 +0900

    ログイン中のユーザーのnew_user_pathへのアクセス制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
              format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
-  has_many :tasks
-  has_secure_password
   validates :password, length: { minimum: 6 }
+  has_secure_password
+  has_many :tasks
 end
